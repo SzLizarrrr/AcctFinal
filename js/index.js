aeaed2df29c4a303b334ffd20a0905d11996ff4c
zhous024 <shengyi.x.zhou.-nd@disney.com>
2016-12-13 12:46:05 -0800

    beautify format

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-$(function()  {
-    $.getJSON('questions/trueOrFalse.json',function(data){
-        var subjects= [];
-        $.each( data, function( key, val ) {
+$(function() {
+    $.getJSON('questions/trueOrFalse.json', function(data) {
+        var subjects = [];
+        $.each(data, function(key, val) {
             subjects.push(
                 "<li id='question_" + key + "'>" + val.question + "</li>"
             );
-            $.each( val.choise, function (i, val) {
+            $.each(val.choise, function(i, val) {
                 subjects.push(
-                  "<li id='choise_" + key + "_" + i +"'>" + val + "</li>"
+                    "<li id='choise_" + key + "_" + i + "'>" + val + "</li>"
                 );
             });
         });
 
-        $( "<ul/>", {
+        $("<ul/>", {
             "class": "my-new-list",
-            html: subjects.join( "" )
-        }).appendTo( "body" );
+            html: subjects.join("")
+        }).appendTo("body");
     });
 });
