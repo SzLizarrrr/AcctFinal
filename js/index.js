09b965eedf83b8efbbb93e78c536e0b426c4091a
SzLizarrrr <yf8189@gmail.com>
2016-12-12 22:49:53 -0800

    structure switch

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function()  {
+    $.getJSON('questions/trueOrFalse.json',function(data){
+        var subjects= [];
+        $.each( data, function( key, val ) {
+            subjects.push(
+                "<li id='question_" + key + "'>" + val.question + "</li>"
+            );
+            $.each( val.choise, function (i, val) {
+                subjects.push(
+                  "<li id='choise_" + key + "_" + i +"'>" + val + "</li>"
+                );
+            });
+        });
+
+        $( "<ul/>", {
+            "class": "my-new-list",
+            html: subjects.join( "" )
+        }).appendTo( "body" );
+    });
+});
